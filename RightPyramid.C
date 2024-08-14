b5fbfc8506bafaa91e06899a67015ae2ebfd519a
AkashKasimani <117535210+AkashKasimani@users.noreply.github.com>
2024-08-14 11:58:23 +0530

    Update RightPyramid.C

--- RightPyramid.C
+++ RightPyramid.C
@@ -1,1 +1,17 @@
-
+#include<stdio.h>
+#include<conio.h>
+void main(){
+int i,j,k,r;
+printf("Enter Rows: ");
+scanf("%d",&r);
+for(i=0;i<=r;i++){
+for(j=i;j<=r;j++){
+printf(" ");
+}
+for(k=0;k<=i;k++){
+printf("*");
+}
+printf("\n");
+}
+getch();
+}
