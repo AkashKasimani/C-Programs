4b1f34b5df94a675e80277921fad990f581d9a50
AkashKasimani <117535210+AkashKasimani@users.noreply.github.com>
2024-09-05 20:39:48 +0530

    Update snake-matrix.c

--- snake-matrix.c
+++ snake-matrix.c
@@ -2,7 +2,10 @@
 #define M 4
 #define N 4
 int main(){
-    int mat[M][N]={0};
+    int mat[M][N]={{1,2,3,4},
+    {5,6,7,8},
+    {9,10,11,12},
+    {13,14,15,16}};
     for(int i=0;i<M;i++){
         for(int j=0;j<N;j++){
                 scanf("%d",&mat[M][N]);
