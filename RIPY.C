b9955deed7bf0cd6764791cd9f2f5f28fb3c0054
AkashKasimani <117535210+AkashKasimani@users.noreply.github.com>
2024-10-14 19:05:26 +0530

    Update RIPY.C

--- RIPY.C
+++ RIPY.C
@@ -1,17 +1,15 @@
 #include<stdio.h>
-#include<conio.h>
 void main(){
-int i,j,k,r;
-printf("Enter R:");
+int i,j,r;
+printf("Enter R :");
 scanf("%d",&r);
 for(i=r-1;i>=0;i--){
-for(j=i;j>=0;j--){
+for(j=i;j<r;j++){
 printf(" ");
 }
-for(k=i;k>=0;k--){
-printf("*");
+for(int k=i;k>=0;k--){
+    printf("*");
 }
 printf("\n");
 }
-getch();
 }
