9b2cff1f577bd540a9400f23c3c3235b6b1979e4
Akash <117535210+AkashKasimani@users.noreply.github.com>
2025-01-08 21:02:03 +0530

    Create Fibonacci with recursion.c

---
+++ Fibonacci with recursion.c
@@ -0,0 +1,17 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+int fib(int i){
+    if(i==0)
+    return 0;
+    else if(i==1)
+    return 1;
+    else
+    return fib(i-1)+fib(i-2);
+}
+
+int main() {
+    int n=5;
+    for(int i=0;i<n;i++){
+        printf("%d,",fib(i));
+    }
+}
